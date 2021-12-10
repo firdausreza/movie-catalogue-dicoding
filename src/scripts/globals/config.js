4300bc6c75d621ddacc0c4c583cb30635e3a4514
Reza Azerly <kiritosao080@gmail.com>
2021-12-10 16:05:48 +0700

    adding cache to service worker

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   KEY: '171b1d2c0a8f4366a4ecae103eb6f4e9',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1'
 }
 
 export default CONFIG;
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,13 +1,18 @@
+import 'regenerator-runtime';
+import CacheHelper from "./utils/cache-helper";
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
   console.log("Installing service worker...");
 });
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
   console.log("Activating service worker...");
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
